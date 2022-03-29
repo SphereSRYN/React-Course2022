b2c94915d28f5b08886aef8d9303b0a446a3f235
SphereSRYN <sphere.sryn@gmail.com>
2022-03-29 12:44:37 +0900

    jsx

--- Learn.md
+++ Learn.md
@@ -1,3 +1,17 @@
 bunch of small pieces to put together to make sth greater
 
 it's likes lego blocks
+
+### Declarative
+
+What should be done?
+=> 뭐해야 하는지 알려주면 내가 알아서 할겡
+
+### Imperative
+
+How should it be done
+=> 어떻게 해야하는지 every step 규율 강령 가이드 명시해줭 그러면 그에따라서 할겡
+
+### js에 html문법을 적용한것 jsx
+
+regular dom 문법이랑 어떻게 다를까?
--- index.js
+++ index.js
@@ -4,10 +4,114 @@ the line of code you just learned! Nope, not on my watch!
 Try to write that 1-liner of React code again! This time,
 see if you can figure out how to render an <ul> with 2+ <li>s inside*/
 
+//vanila js 문법
+const h1 = document.createElement("h1");
+h1.textContent = "This is an imperative way to program";
+h1.className = "header";
+
+console.log(h1);
+//<h1 class="header">
+
+const element = <h1 className="header">This is JSX</h1>;
+console.log(element);
+/*
+Object
+$$typeof: Symbol(react.element)
+key: null
+props: {
+  className: 'header',
+  children: 'This is JSX'}
+ref: null
+type: "h1"
+_owner: null
+_store: {validated: false}
+_self: null
+_source: null
+[[Prototype]]: Object
+ */
+// jsx로 작성하면 retrun 되는건 plain old js 오브젝트인걸 알수있음
+// react가  inperpret할 수 있는 used to create actual element
 function Main({ name }) {
   return <h1>안녕{name}</h1>;
 }
+console.log(Main);
+/* ƒ Main(_ref) {
+  var name = _ref.name;
 
+  return React.createElement(
+    "h1",
+    null,
+    "\uC548\uB155",
+    name
+  );
+}*/
+
+const page = (
+  <div>
+    <h1>h1</h1>
+    <p>p</p>
+  </div>
+); //parenthesis로 감싸줭
+console.log(page);
+/*
+          {
+  type: "div",
+  key: null,
+  ref: null,
+  props: {
+    children: [
+      {
+        type: "h1",
+        key: null,
+        ref: null,
+        props: { className: "header", children: "This is JSX" },
+        _owner: null,
+        _store: {},
+      },
+      {
+        type: "p",
+        key: null,
+        ref: null,
+        props: { children: "This is a paragraph" },
+        _owner: null,
+        _store: {},
+      },
+    ],
+  },
+
+};
+*/
+/* 
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+function Nav_() {
+  return (
+    <nav>
+      <h1>website</h1>
+      <ul>
+        <li>
+          <a href="/">Pricing</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Contact</a>
+        </li>
+      </ul>
+      <input type="search" placeholder="search" />
+      <button type="button">search</button>
+    </nav>
+  );
+}
 function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -88,10 +192,7 @@ function NavBar() {
   );
 }
 
-ReactDOM.render(
-  <div>
-    <NavBar main={Main} />
-    <Main name="ryun" />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Nav_ />, document.getElementById("root"));
+{
+  /* // document.getElementById("root").append(h1) */
+}
