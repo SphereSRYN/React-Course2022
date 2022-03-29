2c4aeb65e11f9e5a779701767a4549a94cd50812
SphereSRYN <sphere.sryn@gmail.com>
2022-03-29 19:16:23 +0900

    e디렉토링ㅣ

---
+++ 02/index.js
@@ -0,0 +1,26 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+import MainContent from "./MainContent";
+import Footer from "./Footer";
+import Header from "./Header";
+/**
+Challenge: 
+
+- Move the `footer` into its own component called "Footer" 
+  and render that component inside the Page component.
+- Move the `h1` and `ol` together into another component
+  called "MainContent" and render inside Page as well.
+*/
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
